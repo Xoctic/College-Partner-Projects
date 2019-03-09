07b0d0337aff6f1112b86b983ff4205ec63609e0
Joseph Zachary <zachary@cs.utah.edu>
2019-03-08 21:59:50 -0700

    Add PS8 solution - experimental and not ready to use

---
+++ PS8/ReaderWriterUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ReaderWriterUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReaderWriterUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ae9afb0e-ccd5-43be-94f0-61ef649a7590")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PS8/ReaderWriterUnitTests/RWLockTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+
+namespace ReaderWriterUnitTests
+{
+    [TestClass]
+    public class RWLockTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(SynchronizationLockException))]
+        public void TestMethod1()
+        {
+            RWLock rwLock = new RWLock();
+            rwLock.EnterReadLock();
+            rwLock.ExitWriteLock();
+        }
+    }
+}
