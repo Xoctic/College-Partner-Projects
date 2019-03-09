d092b2f2ef5c09b89fb9a0bb247c7f3fee170dd6
Aric Campbell <campbell.aric@ymail.com>
2019-03-09 00:07:46 -0700

    dumb

--- Spreadsheet/SpreadsheetGUI/Form1.Designer.cs
+++ Spreadsheet/SpreadsheetGUI/Form1.Designer.cs
@@ -1,13 +1,13 @@
-﻿namespace SSGui
+﻿namespace SpreadsheetGUI
 {
-    partial class SpreadsheetPanel
+    partial class Form1
     {
-        /// <summary> 
+        /// <summary>
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary> 
+        /// <summary>
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
@@ -20,28 +20,21 @@
             base.Dispose(disposing);
         }
 
+        #region Windows Form Designer generated code
 
-
-        #region Component Designer generated code
-
-        /// <summary> 
-        /// Required method for Designer support - do not modify 
+        /// <summary>
+        /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // SpreadsheetPanel
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Name = "SpreadsheetPanel";
-            this.Size = new System.Drawing.Size(1369, 1117);
-            this.ResumeLayout(false);
-
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Form1";
         }
 
         #endregion
     }
 }
+
