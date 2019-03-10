f60d032e7077de5d0c7f33cd24d477c1f90eb5b4
Joseph Zachary <zachary@cs.utah.edu>
2019-03-09 22:17:41 -0700

    Version for use with PS8

--- PS8/ReaderWriterUnitTests/RWLockTests.cs
+++ PS8/ReaderWriterUnitTests/RWLockTests.cs
@@ -1,13 +1,10 @@
 ﻿// Initial version written by Joseph Zachary for CS 3500
 // Copyright Joseph Zachary, March 2019
 
-// Don't test RWLockBuilder class
-// Don't test Dispose
-// Don't use directly the lock constructors
-
 using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Threading;
+using System.Threading.Tasks;
 using ReaderWriterLockClasses;
 
 namespace ReaderWriterUnitTests
@@ -19,15 +16,53 @@ namespace ReaderWriterUnitTests
         /// Verifies that an attempt to exit a write lock without having previously acquired a write
         /// lock results in a SynchronizationLockException.
         /// </summary>
-        [TestMethod]
+        [TestMethod, Timeout(500)]
         [ExpectedException(typeof(SynchronizationLockException))]
         public void TestMethod1()
         {
             // This is how you should create a lock in all of your test cases.
+            // Do not create locks any other way or your code will be ungradeable.
             RWLock rwLock = RWLockBuilder.NewLock();
 
-            // This should result in an exception because a write lock was acquired first
+            // This should result in an exception because a write lock was never acquired.
             rwLock.ExitWriteLock();
+        }
+
+        /// <summary>
+        /// Verifies that two tasks can simultaneously acquire the same read lock.
+        /// </summary>
+        [TestMethod, Timeout(1500)]
+        public void TestMethod2()
+        {
+            // These local variables are used by the GetReadLock method below.  They are accessible
+            // to that method because it is nested within TestMethod2.
+            int count = 2;
+            ManualResetEvent mre = new ManualResetEvent(false);
+            RWLock rwLock = RWLockBuilder.NewLock();
+
+            // Run GetReadLock() on two tasks.  Wait up to one second for count to be decremented to zero.
+            Task t1 = Task.Run(() => GetReadLock());
+            Task t2 = Task.Run(() => GetReadLock());
+            Assert.IsTrue(SpinWait.SpinUntil(() => count == 0, 1000), "Unable to have two simultaneous readers");
+
+            // Allow the blocked tasks to resume, which will result in their termination
+            mre.Set();
+
+            // This method is run simultaneously in two tasks
+            void GetReadLock ()
+            {
+                // Acquire a read lock
+                rwLock.EnterReadLock();
+
+                // Atomically decrement the shared count variable.  Note that merely doing count-- won't always work.
+                Interlocked.Decrement(ref count);
+
+                // Block until the main task sets the mre to true.
+                mre.WaitOne();
+
+                // Exit the read lock
+                rwLock.ExitReadLock();
+            }
         }
     }
 }
