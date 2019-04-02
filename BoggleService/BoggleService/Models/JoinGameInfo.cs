6a5ce6b4570d69c5000f2e33ca39404dd5e8af7c
Andrew <haredog07@yahoo.com>
2019-04-02 17:46:36 -0600

    currently working on PostJoinGame

---
+++ BoggleService/BoggleService/Models/JoinGameInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BoggleService.Models
+{
+    /// <summary>
+    /// Class to store the Game Info when a Join Game request executes.
+    /// </summary>
+    public class JoinGameInfo
+    {
+        /// <summary>
+        /// Stores the time limit being passed into the server.
+        /// </summary>
+        public int timeLimit { get; set; }
+
+        /// <summary>
+        /// Stores the user token being passed into the server.
+        /// </summary>
+        public string userToken { get; set; }
+    }
+}
